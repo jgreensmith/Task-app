dc34c82818d746b9900896dbd3abd50e297a5d0a
James Greensmith <jgreensmith13@gmail.com>
2022-03-22 23:44:49 +0000

    mongodb connected

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import { ExpandMore } from "@mui/icons-material";
+import clientPromise from '../lib/mongodb'
+
 import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Container, IconButton, Stack, toolbarClasses, Typography } from "@mui/material";
 import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
 import Layout from "../components/Layout";
@@ -11,7 +13,8 @@ import CommentIcon from '@mui/icons-material/Comment';
 const images = [];
 
 
-const Home = () => {
+export default function Home({ isConnected }) {
+  console.log(isConnected)
   return (
     <Layout>
       <Container maxWidth="sm">
@@ -82,5 +85,21 @@ const Home = () => {
 }
 
 
-
- export default Home;
+ export async function getServerSideProps(context) {
+  try {
+    // client.db() will be the default database passed in the MONGODB_URI
+    // You can change the database by calling the client.db() function and specifying a database like:
+    // const db = client.db("myDatabase");
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+    await clientPromise
+    return {
+      props: { isConnected: true },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
+  }
+}
