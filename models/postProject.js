b64819bd1de2e4e64d9cd3ccc737850a4e0ec587
James Greensmith <jgreensmith13@gmail.com>
2022-03-23 15:22:15 +0000

    schema

---
+++ models/postProject.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Please add a title'],
+        unique: true,
+        maxlength: [40, 'Title cannot be more than 40 characters']
+    },
+    message: {
+        type: String,
+        required: true,
+        maxlength: [200, 'Description cannot be more than 200 characters']
+    },
+    pals: {
+        type: Number,
+        required: true,
+    },
+    deadline: {
+        date: {type: Date},
+        time: {type: Number}
+    }
+
+
+
+
+})
